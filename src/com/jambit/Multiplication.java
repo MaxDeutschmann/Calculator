91159560bd72f036fff4c81174429414644b8ee5
zomran <55500255+Zain-O@users.noreply.github.com>
2019-10-04 15:03:09 +0200

    add multiblication class

---
+++ src/com/jambit/Multiplication.java
@@ -0,0 +1,10 @@
+package com.jambit;
+
+public class Multiplication {
+    public double multiplication(double firstNumber, double secondNumber) {
+        double result;
+        result = firstNumber * secondNumber;
+        System.out.println(result);
+        return result;
+    }
+}
