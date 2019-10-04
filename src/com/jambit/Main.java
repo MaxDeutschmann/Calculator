d2340f7f751d0a0927c4869756b035bda243a230
Jovan Drcelic <Jovan.Drcelic@jambit.com>
2019-10-04 14:48:46 +0200

    fertig

--- src/com/jambit/Main.java
+++ src/com/jambit/Main.java
@@ -3,6 +3,7 @@ package com.jambit;
 public class Main {
 
     public static void main(String[] args) {
-
+Superclass toto = new Superclass();
+toto.stringLeser();
     }
 }
---
+++ src/com/jambit/Superclass.java
@@ -0,0 +1,19 @@
+package com.jambit;
+
+import java.util.Scanner;
+
+public class Superclass {
+    public void stringLeser(){
+        Scanner toto = new Scanner(System.in);
+        String a = toto.next();
+if (a.contains("+")){
+    //Methode
+}else if (a.contains("-")){
+    //Methode
+}else if (a.contains("*")){
+    //Methode
+}else if (a.contains("/")){
+    //Methode
+}
+    }
+}
