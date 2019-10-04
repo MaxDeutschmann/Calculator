0deec751c0c69a0a3578613a1ed1c0b9b3f238d8
LikeARocket <Max.Deutschmann@jambit.com>
2019-10-04 11:31:54 +0200

    Addition is done

---
+++ src/com/jambit/Addition.java
@@ -0,0 +1,14 @@
+package com.jambit;
+
+import java.util.Scanner;
+
+public class Addition {
+    Scanner sc = new Scanner(System.in);
+
+    public int additionFunction(int a , int b) {
+        int result = a + b;
+        System.out.println(result);
+        return result;
+
+    }
+}
--- src/com/jambit/Main.java
+++ src/com/jambit/Main.java
@@ -3,6 +3,8 @@ package com.jambit;
 public class Main {
 
     public static void main(String[] args) {
+Addition plus = new Addition();
+plus.additionFunction(3,5);
 
     }
 }
