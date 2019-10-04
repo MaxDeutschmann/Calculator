b083ff9468b0e41204abc67051c17dc2ea81578a
LikeARocket <Max.Deutschmann@jambit.com>
2019-10-04 13:19:03 +0200

    Adding the multiplication function (could work)

--- src/com/jambit/Multiplication.java
+++ src/com/jambit/Multiplication.java
@@ -2,11 +2,11 @@ package com.jambit;
 
 import java.util.Scanner;
 
-public class Addition {
+public class Multiplication {
     Scanner sc = new Scanner(System.in);
 
-    public int additionFunction(int a , int b) {
-        int result = a + b;
+    public int multiplicationFunction(int a , int b) {
+        int result = a * b;
         System.out.println(result);
         return result;
 
